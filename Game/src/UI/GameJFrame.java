e24bba76d3b9bea2bcdbb87ba67b90fd07dca7f3
lsyalsy <1669156934@qq.com>
2025-05-13 13:06:08 +0800

    commit

---
+++ Game/src/UI/GameJFrame.java
@@ -0,0 +1,14 @@
+package UI;
+
+import javax.swing.*;
+
+public class GameJFrame extends JFrame {
+    public GameJFrame(){
+        setSize(603, 680);
+        setTitle("拼图游戏");
+        setAlwaysOnTop(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+}
---
+++ Game/src/UI/LonginJFrame.java
@@ -0,0 +1,13 @@
+package UI;
+
+import javax.swing.*;
+
+public class LonginJFrame extends JFrame {
+    public LonginJFrame(){
+        setSize(503, 580);
+        setAlwaysOnTop(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+}
