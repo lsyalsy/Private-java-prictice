c72fc60af9df56e453ed6e9cdb7dea53af557fe0
lsyalsy <1669156934@qq.com>
2025-05-13 13:06:08 +0800

    second commit

---
+++ Game/src/UI/APP.java
@@ -0,0 +1,7 @@
+package UI;
+
+public class APP {
+    public static void main(String[] args) {
+        new GameJFrame();
+    }
+}
---
+++ Game/src/UI/RegisterJFrame.java
@@ -0,0 +1,13 @@
+package UI;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame(){
+        setSize(503, 580);
+        setAlwaysOnTop(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+}
