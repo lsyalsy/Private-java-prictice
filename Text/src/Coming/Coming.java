28d0fd52efe0886e5796e36bd73ae607925040d6
lsyalsy <1669156934@qq.com>
2025-05-31 00:00:44 +0800

    test

--- Text/src/Coming/Coming.java
+++ Text/src/Coming/Coming.java
@@ -2,6 +2,8 @@ package Coming;
 
 public class Coming {
     public static void main(String[] args) {
+
         System.out.println("I`m coming");
+        System.out.println("I`m coming again");
     }
 }
