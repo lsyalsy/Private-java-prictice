1fdff2477dfac1701422fd2de7fd6ac9d127dde6
lsyalsy <1669156934@qq.com>
2025-06-10 19:41:59 +0800

    commit

--- Homework.01/src/Inverted/Inverted.java
+++ Homework.01/src/Inverted/Inverted.java
@@ -1,25 +1,21 @@
 package Inverted;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 //倒序输出数字
 public class Inverted {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入一个整数：");
-        int n = sc.nextInt(), count = 0;
-        int[] arr = new int[22];
-        if (n == 0) {
-            System.out.println("0");
+        int n = sc.nextInt();
+        while(n > 0){
+            list.add(n % 10);
+            n /= 10;
         }
-        else {
-            while (n > 0) {
-                arr[count] = n % 10;
-                n /= 10;
-                count++;
-            }
-        }
-        for (int i = 0; i < count; i++) {
-            System.out.print(arr[i]);
+        System.out.print("倒序为：");
+        for(int num : list){
+            System.out.print(num);
         }
     }
 }
