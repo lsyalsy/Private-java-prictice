e42efa58365081fc2743f14ecbb7eac6459523bb
lsyalsy <1669156934@qq.com>
2025-07-19 12:30:58 +0800

    commit

--- SMU/src/test/Find.java
+++ SMU/src/test/Find.java
@@ -1,45 +1,47 @@
 package test;
-import java.util.*;
-public class Find {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Find {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int[] arr = new int[n];
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
         }
 
-        int num = 0;
+        tokenizer = new StringTokenizer(reader.readLine());
+        StringBuilder resultBuilder = new StringBuilder();
         while (true) {
-            num = sc.nextInt();
-            if(num == -1) break;
-            boolean exsit = false;
-            for (int i = 0; i < n; i++) {
-                if (arr[i] == num) {
-                    exsit = true;
-                    break;
-                }
-            }
-            if(exsit) {
-
-                int left = 0, right = n - 1;
-                while (left <= right) {
-                    int mid = left + right >>> 1;
-                    if (arr[mid] == num) {
-                        left = mid;
-                        break;
-                    } else if (arr[mid] < num) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
-                }
-                System.out.print((left + 1) + " ");
-            }else {
-                System.out.println(0);
+            int target = Integer.parseInt(tokenizer.nextToken());
+            if (target == -1) {
                 break;
             }
+            int result = binarySearch(arr, target);
+            resultBuilder.append(result).append(" ");
         }
+        System.out.println(resultBuilder.toString());
+        reader.close();
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
     }
 }
